0fb034560a5edb69286099876556ceb080310155
suhani0343 <suhaninegi15@gmail.com>
2026-07-04 14:19:48 +0530

    Feat: Complete Day 1 Hello World boilerplate

---
+++ 01_hello_world.cpp
@@ -0,0 +1,9 @@
+/* Goal: Got my first programming running.
+Concepts Learned: Boilerplate syntax,streams(cout), and basic compilation.
+*/
+#include<iostream>
+using namespace std; // we can use std::cout directly
+int main(){
+    cout<<" Hello World!!\n I am Suhani Negi";
+    return 0;
+}
